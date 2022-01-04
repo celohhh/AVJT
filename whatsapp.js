0dfb8808bbc3d9a7b68cf27599bf4756590fd0e0
SisenandoCalixto <70386406+SisenandoCalixto@users.noreply.github.com>
2022-01-04 19:04:35 -0300

    Delete whatsapp.js

--- whatsapp.js
+++
@@ -1,9 +0,0 @@
-function whatsappEscreverMensagem(
-  telefone  = '',
-  texto     = ''
-){
-
-  let url	=	LINK.whatsapp.protocolo + numeros(telefone) + '&text=' + encodeURI(texto)
-  window.location.href = url
-
-}
